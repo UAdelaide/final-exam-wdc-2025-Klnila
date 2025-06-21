1b423e1c2dc5e82a1c55f4105c3731e56695aefb
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 <a1908705@adelaide.edu.au>
2025-06-21 04:13:33 +0000

    [automatic commit] 2025-06-21 04:13:33 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -53,3 +53,35 @@ CREATE TABLE WalkRatings (
     FOREIGN KEY (owner_id) REFERENCES Users(user_id),
     CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
 );
+INSERT INTO Users (username, email, password_hash, role)
+
+VALUES
+
+(' alice123' ,  'alice@example.com',  'hashed123',  'owner'),
+
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('davidwalker', 'david@example.com', 'hashed000', 'walker'),
+('emilyowner', 'emily@example.com', 'hashed999', 'owner');
+
+
+
+
+INSERT INTO Dogs (name, size, owner_id)
+VALUES
+  ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+  ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+  ('Poppy', 'large', (SELECT user_id FROM Users WHERE username = 'nila223')),
+  ('Shadow', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+  ('Gypsy', 'small', (SELECT user_id FROM Users WHERE username = 'anna123'));
+
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+
+VALUES
+
+
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'Hilltop Park', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Luna'), '2025-06-12 14:00:00', 30, 'Riverside Walk', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-13 16:30:00', 40, 'City Gardens', 'cancelled');
