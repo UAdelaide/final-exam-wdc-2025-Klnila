be968926d67df5c073dd0ae5b28035e0ed99728d
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 <a1908705@adelaide.edu.au>
2025-06-21 04:14:35 +0000

    [automatic commit] 2025-06-21 04:14:34 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -55,8 +55,7 @@ CREATE TABLE WalkRatings (
 );
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
-('alice123', 'alice@example.com','hashed123',  'owner'),
-
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('davidwalker', 'david@example.com', 'hashed000', 'walker'),
@@ -67,10 +66,10 @@ VALUES
 
 INSERT INTO Dogs (name, size, owner_id)
 VALUES
-  ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
-  ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
-  ('Poppy', 'large', (SELECT user_id FROM Users WHERE username = 'nila223')),
-  ('Shadow', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+('Poppy', 'large', (SELECT user_id FROM Users WHERE username = 'nila223')),
+('Shadow', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
   ('Gypsy', 'small', (SELECT user_id FROM Users WHERE username = 'anna123'));
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
